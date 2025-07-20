8e65f5da433abaeffd49425778a722fddf82c568
Vinay Kumar <vinayjangra.dev@gmail.com>
2025-07-20 12:50:09 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+
+import Editor from './Components/Editor'
+
+function App() {
+
+  return (
+    <Editor />
+     
+  )
+}
+
+export default App
---
+++ client/src/Components/Editor.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+
+
+const Editor: React.FC = () => {
+    return <div>Hello from vinay</div>
+}
+
+
+
+export default Editor;
