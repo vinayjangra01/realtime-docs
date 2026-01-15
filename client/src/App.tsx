c82ce2fc82a23164fb8bff9db69f61dd9ec938bb
Vinay Kumar <vinayjangra.dev@gmail.com>
2026-01-16 02:34:44 +0530

    feat: implement initial real-time document editor client with Quill, Socket.IO, and React Router.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
+import './App.css';
 
-import Editor from './Components/Editor'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
+
+import Editor from './Components/Editor';
 
 function App() {
-
   return (
-    <Editor />
-     
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate replace to={`/docs/${uuid()}`} /> } />
+        <Route path='/docs/:id' element={<Editor />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- client/src/Components/Editor.tsx
+++ client/src/Components/Editor.tsx
@@ -1,10 +1,120 @@
-import type React from "react";
+import { useEffect, useState } from 'react';
+
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+
+import { Box } from '@mui/material';
+import styled from '@emotion/styled';
 
 
-const Editor: React.FC = () => {
-    return <div>Hello from vinay</div>
+import { io, Socket } from 'socket.io-client';
+import { useParams } from 'react-router-dom';
+import { Delta } from 'quill';
+
+const Component = styled.div`
+    background: #F5F5F5;
+`
+
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+];
+  
+
+const Editor = () => {
+    const [socket, setSocket] = useState<Socket | null>(null);
+    const [quill, setQuill] = useState<Quill | null>(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const quillServer = new Quill('#container', { theme: 'snow', modules: { toolbar: toolbarOptions }});
+        quillServer.disable();
+        quillServer.setText('Loading the document...')
+        setQuill(quillServer);
+    }, []);
+
+    useEffect(() => {
+        const socketServer = io('http://localhost:9000');
+        setSocket(socketServer);
+
+        return () => {
+            socketServer.disconnect();
+        }
+    }, [])
+
+    useEffect(() => {
+        if (socket === null || quill === null) return;
+
+        const handleChange = (delta: Delta, oldData: Delta, source: string) => {
+            if (source !== 'user') return;
+
+            socket.emit('send-changes', delta);
+        }
+
+        quill.on('text-change', handleChange);
+
+        return () => {
+            quill.off('text-change', handleChange);
+        }
+    }, [quill, socket])
+
+    useEffect(() => {
+        if (socket === null || quill === null) return;
+
+        const handleChange = (delta: Delta) => {
+            quill.updateContents(delta);
+        }
+
+        socket.on('receive-changes', handleChange);
+
+        return () => {
+            socket.off('receive-changes', handleChange);
+        }
+    }, [quill, socket]);
+
+    useEffect(() => {
+        if (quill === null || socket === null) return;
+
+        socket.once('load-document', document => {
+            quill.setContents(document);
+            quill.enable();
+        })
+
+        socket.emit('get-document', id);
+    },  [quill, socket, id]);
+
+    useEffect(() => {
+        if (socket === null || quill === null) return;
+
+        const interval = setInterval(() => {
+            socket.emit('save-document', quill.getContents())
+        }, 2000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [socket, quill]);
+
+    return (
+        <Component>
+            <Box className='container' id='container'></Box>
+        </Component>
+    )
 }
-
-
 
 export default Editor;
